b001c0ea2b4de138ad6a597951c4ce5af05dace9
Yoshihide Jimbo <yjimbo@gmail.com>
2022-12-29 17:34:53 +0900

    Update astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.my-site.dev', // Your final, deployed URL
+  base: '/foo', // The base path to deploy to
+});
