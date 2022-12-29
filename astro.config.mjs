124dd1c43110ac6d8f0e799c518b29c2efcbf2f0
Yoshihide Jimbo <yjimbo@gmail.com>
2022-12-29 17:38:30 +0900

    Install @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  site: 'https://www.my-site.dev', // Your final, deployed URL
-  base: '/foo', // The base path to deploy to
+  // Your final, deployed URL
+  site: 'https://www.my-site.dev',
+  // The base path to deploy to
+  base: '/foo',
+  integrations: [sitemap()],
 });
