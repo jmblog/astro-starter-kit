3d356d4e2f3fe9c299fb34ffd13e7c8eb78c88a7
Yoshihide Jimbo <yjimbo@gmail.com>
2022-12-29 17:40:35 +0900

    Install astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
 
 export default defineConfig({
   // Your final, deployed URL
   site: 'https://www.my-site.dev',
   // The base path to deploy to
   base: '/foo',
-  integrations: [sitemap()],
+  integrations: [sitemap(), compress()],
 });
